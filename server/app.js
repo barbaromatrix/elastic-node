d93d1984603047191d42fb5ea939afcc4b196263
Matheus Galdino <matheus.galdino@hotmail.com>
2018-03-01 14:12:14 -0300

    Merged from feature/front-application

--- server/app.js
+++ server/app.js
@@ -10,10 +10,10 @@ const port = process.env.PORT || 3000;
 
 
 app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}`);
 });
 
 // Log percolated errors to the ocnsole
@@ -21,8 +21,8 @@ app.on('error', err => console.log(`Error: ${err}`));
 
 // Set permissive CORS header
 app.use(async (ctx, next) => {
-  ctx.set('Access-Control-Allow-Origin', '*');
-  return next();
+    ctx.set('Access-Control-Allow-Origin', '*');
+    return next();
 });
 
 /**
@@ -34,21 +34,43 @@ app.use(async (ctx, next) => {
  */
 router.get('/search', validate({
     query: {
-      term: joi.string().max(60).required(),
-      offset: joi.number().integer().min(0).default(0)
+        term: joi.string().max(60).required(),
+        offset: joi.number().integer().min(0).default(0)
     }
-  }),
-  async (ctx, next) => {
-    const { term, offset } = ctx.request.query;
+}),
+    async (ctx, next) => {
+        const { term, offset } = ctx.request.query;
 
-    ctx.body = await search.queryTerm(term, offset);
-});
+        ctx.body = await search.queryTerm(term, offset);
+    });
+
+/**
+ * GET /paragraphs
+ * Get a range of paragraphs from the specified book
+ * Query Params -
+ * bookTitle: string under 256 characters
+ * start: positive integer
+ * end: positive integer greater than start
+ */
+router.get('/paragraphs',
+    validate({
+        query: {
+            bookTitle: joi.string().max(256).required(),
+            start: joi.number().integer().min(0).default(0),
+            end: joi.number().integer().greater(joi.ref('start')).default(10)
+        }
+    }),
+    async (ctx, next) => {
+        const { bookTitle, start, end } = ctx.request.query
+        ctx.body = await search.getParagraphs(bookTitle, start, end)
+    }
+);
 
 app
-  .use(router.routes())
-  .use(router.allowedMethods())
-  .listen(port, err => {
-    if (err) console.error(err);
+    .use(router.routes())
+    .use(router.allowedMethods())
+    .listen(port, err => {
+        if (err) console.error(err);
 
-    console.log(`App Listening on Port ${port}`);
-  });
+        console.log(`App Listening on Port ${port}`);
+    });
