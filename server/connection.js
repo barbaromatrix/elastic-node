bcc4149de03b424fc0ff6741b7c4ecc4da4757bf
Matheus Galdino <matheus.galdino@hotmail.com>
2018-03-01 11:48:56 -0300

    Connection with ES docker

--- server/connection.js
+++ server/connection.js
@@ -1,51 +1,53 @@
-const elasticsearch = require('elasticsearch');
+const elasticsearch = require('elasticsearch')
 
+// Core ES variables for this project
 const index = 'library';
-const type = 'novel';
+const type = 'book';
 const port = 9200;
-const host = process.env.PORT || 'localhost';
-const client = new elasticsearch.Client({
-    host: { host, port }
+const host = process.env.ES_HOST || 'localhost';
+const client = new elasticsearch.Client({ 
+    host: { host, port } 
 });
 
-async function checkConnection() {
-    let isConnected = false;
+/** Check the ES connection status */
+async function checkConnection () {
+  let isConnected = false;
 
-    while (!isConnected) {
-        console.log('Connecting to ES');
-        try {
-            const health = await client.cluster.health({});
-            console.log(health);
-            isConnected = true;
-        } catch (err) {
-            console.log('Connection Failed, Retrying...', err);
-        }
+  while (!isConnected) {
+    console.log('Connecting to ES');
+
+    try {
+      const health = await client.cluster.health({});
+      console.log('Health: ', health);
+      isConnected = true;
+    } catch (err) {
+      console.log('Connection Failed, Retrying...', err);
     }
+  }
+}
+
+/** Clear the index, recreate it, and add mappings */
+async function resetIndex () {
+  if (await client.indices.exists({ index })) {
+    await client.indices.delete({ index });
+  }
+
+  await client.indices.create({ index });
+  await putBookMapping();
 }
 
 /** Add book section schema mapping to ES */
-async function putBookMapping() {
-    const schema = {
-        title: { type: 'keyword' },
-        author: { type: 'keyword' },
-        location: { type: 'integer' },
-        text: { type: 'text' }
-    };
+async function putBookMapping () {
+  const schema = {
+    title: { type: 'keyword' },
+    author: { type: 'keyword' },
+    location: { type: 'integer' },
+    text: { type: 'text' }
+  };
 
-    return client.indices.putMapping({ index, type, body: { properties: schema } });
+  return client.indices.putMapping({ index, type, body: { properties: schema } });
 }
-
-
-async function resetIndex() {
-    if (await client.indices.exists({ index })) {
-        await client.indices.delete({ index });
-    }
-
-    await client.indices.create({ index });
-    await putBookMapping();
-}
-
 
 module.exports = {
-    client, index, type, checkConnection, resetIndex
-};
+  client, index, type, checkConnection, resetIndex
+}
